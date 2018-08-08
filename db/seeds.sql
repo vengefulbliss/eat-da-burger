eb8122f9c78d4c2123c2838cac95a93ba37670d0
vengefulbliss <zapyaplee@gmail.com>
2018-08-07 19:41:30 -0600

    burger time

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Buffalo Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Chicken Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom and Swiss Burger", TRUE);
