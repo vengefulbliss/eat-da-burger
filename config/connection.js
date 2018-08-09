fc57fb31399785af90a29bda49098eafee348755
vengefulbliss <zapyaplee@gmail.com>
2018-08-09 14:32:06 -0600

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
+
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+	connection = mysql.createConnection({
 	host:'localhost',
 	port: 3306,
 	user:'root',
@@ -15,5 +23,5 @@ connection.connect(function(err) {
 	}
 	console.log("connected as id " + connection.threadId);
 });
-
+}
 module.exports = connection;
